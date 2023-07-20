297839e7a06edb36ca860e1e55b0f3b061119b6f
avilla13 <xetox13@gmail.com>
2023-07-20 16:12:13 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+# Exercise 1
+
+students = [
+    'Alex',
+    'Aaron',
+    'Andrew',
+    'Carneala',
+    'Cesar'
+]
+
+print(f"The second student's name is {students[1]}")
+
+# Exercise 2
+
+
+# Exercise 3
+
+
+# Exercise 4
+
+
+# Exercise 5
+
+
+# Exercise 6
+
+
+# Exercise 7
+
+
+# Exercise 8
+
+
+
+
+
